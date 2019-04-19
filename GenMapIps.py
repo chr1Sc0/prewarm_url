7f9abc8c1210b35e8d3db8f726fb218808f19cf9
Christian Scotti <cscotti@akamai.com>
2019-04-19 20:21:19 +0200

    Add Europe countries filtering

---
+++ GenMapIps.py
@@ -0,0 +1,75 @@
+#!/usr/local/bin/python
+
+import requests
+import os
+import bs4
+import threading
+import csv
+import socket
+import dns.resolver
+import re
+
+CSV_COL_IP = "ip"
+CSV_COL_COUNTRY = "country_id"
+TARGET_HOSTNAME = 'static.zara.net'
+EU_COUNTRIES_ISO = ['AL', 'AD', 'AT', 'BY', 'BE', 'BA', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FO', 'FI', 'FR', 'DE', 'GI', 'GR', 'HU', 'IS', 'IE', 'IM', 'IT',
+                    'RS', 'LV', 'LI', 'LT', 'LU', 'MK', 'MT', 'MD', 'MC', 'ME', 'NL', 'NO', 'PL', 'PT', 'RO', 'RU', 'SM', 'RS', 'SK', 'SI', 'ES', 'SE', 'CH', 'UA', 'GB', 'VA', 'RS']
+
+
+def download_ns_file(url):
+    print('Downloading CSV file...')
+    response = requests.get(url)
+    response.raise_for_status()
+    csvfile = open('nameservers.csv', 'wb')
+    for chunk in response.iter_content(100000):
+        csvfile.write(chunk)
+    csvfile.close
+    print('nameservers file downloaded')
+
+
+def extract_dns_servers(dnsfile):
+    with open(dnsfile) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        ip_pattern = re.compile(
+            '(?:^|\b(?<!\.))(?:1?\d\d?|2[0-4]\d|25[0-5])(?:\.(?:1?\d\d?|2[0-4]\d|25[0-5])){3}(?=$|[^\w.])')
+
+        line_count = 0
+
+        for row in csv_reader:
+            if line_count > 0:
+                if ip_pattern.match(row[CSV_COL_IP]) and str(row[CSV_COL_COUNTRY]) in EU_COUNTRIES_ISO:
+                    print(f'{row[CSV_COL_IP]},{row[CSV_COL_COUNTRY]}')
+                    check_nameserver_ip(row[CSV_COL_IP], TARGET_HOSTNAME)
+            line_count += 1
+        print(f'Processed {line_count} lines.')
+
+
+def check_nameserver_ip(dns_ip, hostname):
+    # Set the DNS Server
+    from dns.resolver import NoAnswer, NXDOMAIN, NoNameservers, Timeout
+
+    a_records = []
+
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = [socket.gethostbyname(dns_ip)]
+    try:
+        for rdata in resolver.query(hostname, 'A'):
+            a_records.append(str(rdata))
+
+    except NoAnswer:
+        a_records = []
+    except NXDOMAIN:
+        a_records = []
+    except NoNameservers:
+        a_records = []
+    except Timeout:
+        a_records = []
+    print(','.join(a_records))
+
+
+if __name__ == "__main__":
+
+    url = "https://public-dns.info/nameservers.csv"
+    download_ns_file(url)
+    extract_dns_servers('nameservers.csv')
